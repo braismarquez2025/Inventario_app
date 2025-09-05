c8f0e30bc51a5714f5a8a20c73725cf5312cfd02
Brais <braismarquez03@gmail.com>
2025-09-05 13:50:19 +0200

    update

--- inventario/settings.py
+++ inventario/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-nc-gae&0u$jt1bo0p94frp(o^42xdmi)d!&w0j6p8a-ojd54q8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['inventario.store']
 
 
 # Application definition
